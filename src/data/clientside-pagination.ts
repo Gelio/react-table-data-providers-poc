6310f0365b2cfe6bd6aaaa193036006fc2a91664
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2020-06-06 17:37:25 +0200

    split clientside operations into smaller functions

--- src/data/clientside-pagination.ts
+++ src/data/clientside-pagination.ts
@@ -1,4 +1,4 @@
-import { always } from 'ramda';
+import { always, identity } from 'ramda';
 import { combineLatest, BehaviorSubject, Observable } from 'rxjs';
 import {
   distinctUntilKeyChanged,
@@ -17,7 +17,9 @@ import { TableDataGetter, TableDataParams, TableData } from './types';
 export const getClientsidePaginatedDataFactory = <RowData>(
   getData: TableDataGetter<RowData>,
   // NOTE: used for filtering, searching, etc.
-  filterData: (row: RowData, searchPhrase: string) => boolean = always(true)
+  filterData: (row: RowData, searchPhrase: string) => boolean = always(true),
+  // TODO: handle `sortingRules` as a 2nd parameter
+  sortData: (rows: RowData[]) => RowData[] = identity
 ): TableDataGetter<RowData> => {
   const paramsSubject = new BehaviorSubject<TableDataParams | null>(null);
   const params$ = paramsSubject.pipe(filter((x) => !!x)) as Observable<
@@ -28,39 +30,14 @@ export const getClientsidePaginatedDataFactory = <RowData>(
     switchMap(getData),
     share()
   );
-  const searchPhrase$ = params$.pipe(
-    pluck('searchPhrase'),
-    distinctUntilChanged()
-  );
-  const filteredTableRows$ = combineLatest(
-    allTableData$.pipe(
-      pluck('rows'),
-      filter((rows) => Array.isArray(rows))
-    ),
-    searchPhrase$
-  ).pipe(
-    map(([rows, searchPhrase]) =>
-      // NOTE: filtered that rows are an array before
-      rows!.filter((row) => filterData(row, searchPhrase))
-    )
+  const filteredTableRows$ = getFilteredTableRows(
+    params$,
+    allTableData$,
+    filterData
   );
 
-  // TODO: searching based on filteredTableRows$
-
-  const page$ = params$.pipe(pluck('page'), distinctUntilChanged());
-  const pageSize$ = params$.pipe(pluck('pageSize'), distinctUntilChanged());
-  const currentPageRows$ = combineLatest(
-    filteredTableRows$,
-    page$,
-    pageSize$
-  ).pipe(
-    map(([rows, page, pageSize]) => {
-      const pageStartIndex = (page - 1) * pageSize;
-      const pageEndIndex = pageStartIndex + pageSize;
-
-      return rows.slice(pageStartIndex, pageEndIndex);
-    })
-  );
+  const sortedTableRows$ = filteredTableRows$.pipe(map(sortData));
+  const currentPageRows$ = getCurrentPageRows(params$, sortedTableRows$);
 
   const currentPageTableData$ = combineLatest(
     allTableData$,
@@ -86,3 +63,41 @@ export const getClientsidePaginatedDataFactory = <RowData>(
     return currentPageTableData$;
   };
 };
+
+function getCurrentPageRows<RowData>(
+  params$: Observable<TableDataParams>,
+  tableRows$: Observable<RowData[]>
+) {
+  const page$ = params$.pipe(pluck('page'), distinctUntilChanged());
+  const pageSize$ = params$.pipe(pluck('pageSize'), distinctUntilChanged());
+
+  return combineLatest(tableRows$, page$, pageSize$).pipe(
+    map(([rows, page, pageSize]) => {
+      const pageStartIndex = (page - 1) * pageSize;
+      const pageEndIndex = pageStartIndex + pageSize;
+
+      return rows.slice(pageStartIndex, pageEndIndex);
+    })
+  );
+}
+
+function getFilteredTableRows<RowData>(
+  params$: Observable<TableDataParams>,
+  allTableData$: Observable<TableData<RowData>>,
+  filterData: (row: RowData, searchPhrase: string) => boolean
+) {
+  const allRows$ = allTableData$.pipe(
+    pluck('rows'),
+    filter((rows) => Array.isArray(rows))
+  ) as Observable<RowData[]>;
+  const searchPhrase$ = params$.pipe(
+    pluck('searchPhrase'),
+    distinctUntilChanged()
+  );
+
+  return combineLatest(allRows$, searchPhrase$).pipe(
+    map(([rows, searchPhrase]) =>
+      rows.filter((row) => filterData(row, searchPhrase))
+    )
+  );
+}
