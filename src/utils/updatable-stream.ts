b512a50c4826f0d1148802ef283b8c7da4049469
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2020-06-07 12:02:08 +0200

    allow using updaters in updatable streeams

--- src/utils/updatable-stream.ts
+++ src/utils/updatable-stream.ts
@@ -1,22 +1,29 @@
 import { Observable, Subject } from 'rxjs';
 import { scan, shareReplay, startWith } from 'rxjs/operators';
 
+export type Updater<T> = (data: T) => T;
+
 export interface UpdatableStream<T> {
   stream$: Observable<T>;
-  // TODO: updater function instead of patch
-  update(patch: Partial<T>): void;
+  update(patchOrUpdater: Partial<T> | Updater<T>): void;
 }
 
 export const getUpdatableStream = <T>(initialValue: T): UpdatableStream<T> => {
-  const updatesSubject = new Subject<Partial<T>>();
+  const updatesSubject = new Subject<Partial<T> | Updater<T>>();
   const stream$ = updatesSubject.pipe(
     startWith({}),
-    scan((data, updates) => ({ ...data, ...updates }), initialValue),
+    scan(
+      (data, patchOrUpdater) =>
+        typeof patchOrUpdater === 'function'
+          ? patchOrUpdater(data)
+          : { ...data, ...patchOrUpdater },
+      initialValue
+    ),
     shareReplay(1)
   );
 
   return {
     stream$,
-    update: (patch) => updatesSubject.next(patch),
+    update: (patchOrUpdater) => updatesSubject.next(patchOrUpdater),
   };
 };
