c85168d7882500716857ccb60de417cce24f0951
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2020-06-08 09:44:38 +0200

    fix ts error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState, useEffect } from 'react';
+import React, { useMemo, useState } from 'react';
 import './App.css';
 
 import { useObservable } from './utils/use-observable';
